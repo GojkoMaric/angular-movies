677b6ae83c02ed04437388bd4b07f18eca3dca7a
GojkoMaric <gojko.maric@yahoo.com>
2018-01-25 19:00:43 +0100

    2nd exam -> sharedModule, movie model, movie service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { SharedModule } from './shared/shared.module',
 
 const appRoutes: Routes = [
   {
@@ -29,7 +30,8 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes
-    )
+    ),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MovieService } from './services/movie.service';
+
+
+@NgModule({
+    imports:[],
+    providers: [
+        MovieService
+    ],
+    declarations: [],
+    exports: []
+})
+
+export class SharedModule { }
