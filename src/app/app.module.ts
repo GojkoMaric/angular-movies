c3e3850775feb8749aa580b5f7ff35965d99b905
GojkoMaric <gojko.maric@yahoo.com>
2018-01-25 18:45:04 +0100

    1st exam -> bootstrap, layout, route, movie component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
